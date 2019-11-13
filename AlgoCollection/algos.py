899a8ff4e5b9cae64dec5786fcc0415b65309bc4
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-11-13 16:32:51 -0500

    add concrete methods

---
+++ AlgoCollection/algos.py
@@ -0,0 +1,14 @@
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
+def pca(X, n_components=2):
+    _pca = PCA(n_components=n_components)
+    embeded = _pca.fit_transform(X)
+    return embeded
+
+
+def tsne(X, n_components=2, perplexity=30, learning_rate=200):
+    _tsne = TSNE(ncomponents=n_components, perplexity=perplexity, learning_rate=learning_rate)
+    embeded = _tsne.fit_transform(X)
+    return embeded

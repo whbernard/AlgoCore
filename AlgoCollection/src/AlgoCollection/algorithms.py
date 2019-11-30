e47cd7177aadfd99917a15499a703de28fa8d4d6
William Bernard <whb@teleapp.com>
2019-11-30 18:15:27 -0500

    Added helper function for UMAP in algorithms.py

--- AlgoCollection/src/AlgoCollection/algorithms.py
+++ AlgoCollection/src/AlgoCollection/algorithms.py
@@ -1,5 +1,6 @@
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from umap import UMAP
 
 
 def pca(X, n_components=2):
@@ -12,3 +13,9 @@ def tsne(X, n_components=2, perplexity=30, learning_rate=200):
     _tsne = TSNE(n_components=n_components, perplexity=perplexity, learning_rate=learning_rate)
     embeded = _tsne.fit_transform(X)
     return embeded
+
+
+def umap(X):
+    _umap = UMAP()
+    embedded = _umap.fit_transform(X)
+    return embedded

5d8b871acc64238febde5d18fb295e1964a929d5
shipengcheng1230 <shipengcheng1230@gmail.com>
2019-12-03 18:30:16 -0500

    1. change json structure for visualizer 2. preprosses data with `nan`

--- AlgoCollection/src/AlgoCollection/algorithms.py
+++ AlgoCollection/src/AlgoCollection/algorithms.py
@@ -3,19 +3,19 @@ from sklearn.manifold import TSNE
 from umap import UMAP
 
 
-def pca(X, n_components=2):
+def pca(X, n_components=3):
     _pca = PCA(n_components=n_components)
     embeded = _pca.fit_transform(X)
     return embeded
 
 
-def tsne(X, n_components=2, perplexity=30, learning_rate=200):
+def tsne(X, n_components=3, perplexity=30, learning_rate=200):
     _tsne = TSNE(n_components=n_components, perplexity=perplexity, learning_rate=learning_rate)
     embeded = _tsne.fit_transform(X)
     return embeded
 
 
-def umap(X, n_components=2):
+def umap(X, n_components=3):
     _umap = UMAP(n_components=n_components)
     embedded = _umap.fit_transform(X)
     return embedded
